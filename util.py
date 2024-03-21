ca80a6be27cd0ab8857ec106f54280301a97f5ec
mchau <minhtriet09@gmail.com>
2024-03-21 01:27:59 +0000

    add util

---
+++ util.py
@@ -0,0 +1,36 @@
+import pennylane as qml
+import copy
+from typing import List
+
+def unitary_fold(circuit, scale_factor: int):
+    # original ops
+    circuit()
+    original_ops = circuit.tape.operations
+    ops = circuit.tape.copy(copy_operations=True).operations
+    n, s = divmod(scale_factor - 1, 2)
+    # take all the gate, append and complex conjugate
+    # For the 1st part  (U^H U)**n
+    for i in range(n):
+        for op in original_ops[::-1]:
+            ops.append(qml.adjoint(copy.copy(op)))
+
+        for op in original_ops:
+            ops.append(op)
+
+    # For the 2nd part (L_d^H .. L_s^H) (L_s .. L_d)
+    if s > 0:
+        last_layers = original_ops[-s:]
+        for op in last_layers[::-1]:
+            ops.append(qml.adjoint(copy.copy(op)))
+        for i in last_layers:
+            ops.append(op)
+
+            # Return list of op to create the circuit
+    return ops, circuit.tape.measurements
+
+
+@qml.qnode(dev)
+def circuit_from_ops(operations: List, measurements: List):
+    for op in operations:
+        qml.apply(op)
+    return qml.apply(measurements[0])
