3f0e5f6f92527cda814a02b8e61668a1a5a9e917
minhtriet <minhtriet09@gmail.com>
2024-03-27 01:24:41 +0100

    some explanation

--- test/test_depolarization_noise.py
+++ test/test_depolarization_noise.py
@@ -10,19 +10,9 @@ class TestCircuitCustom(unittest.TestCase):
         self.dev = qml.device("default.mixed", wires=2)
 
     def test_circuit_custom(self):
-        # Define the quantum circuit function
-        @qml.qnode(self.dev)
-        def circuit_custom(p):
-            qml.Hadamard(wires=0)
-            qml.CNOT(wires=[0, 1])
-            qml.QubitChannel(util.depolarization_kraus_matrices(p), wires=0)
-            qml.QubitChannel(util.depolarization_kraus_matrices(p), wires=1)
-            return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))
-
-        # Set the parameter value for testing
         ps = [0., 0.001, 0.01, 0.1, 0.2]
-        expected = [1., 0.997335, 0.973511, 0.751111, 0.537778]
-        # Execute the quantum circuit
-        for i, p in enumerate(ps):
-            result = circuit_custom(p)
-            assert np.isclose(result, expected[i])
+        for p in ps:
+            dev_noise = qml.transforms.insert(self.dev, qml.DepolarizingChannel, p)
+            result = qml.QNode(util.circuit_bell_state_noisy, self.dev)(p)
+            expect = qml.QNode(util.circuit_bell_state, dev_noise)()
+            assert np.isclose(result, expect), f"Expected {expect}, received {result}"
